bbdd80be7f68817a619ccd4aab8f7bbd8f863eda
odin.dev <thisadee.burom@gmail.com>
2025-07-01 10:43:56 +0700

    WIP Setup Ready for sdtarting

---
+++ lib/models/category_model.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'category_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CategoryModelImpl _$$CategoryModelImplFromJson(Map<String, dynamic> json) =>
+    _$CategoryModelImpl(
+      tableName: json['table_name'] as String?,
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+      description: json['description'] as String?,
+      active: json['active'] as bool?,
+      createdAt: json['created_at'] as String?,
+      updatedAt: json['updated_at'] as String?,
+      createdBy: json['created_by'] == null
+          ? null
+          : UserModel.fromJson(json['created_by'] as Map<String, dynamic>),
+      updatedBy: json['updated_by'] == null
+          ? null
+          : UserModel.fromJson(json['updated_by'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$CategoryModelImplToJson(_$CategoryModelImpl instance) =>
+    <String, dynamic>{
+      'table_name': instance.tableName,
+      'id': instance.id,
+      'name': instance.name,
+      'description': instance.description,
+      'active': instance.active,
+      'created_at': instance.createdAt,
+      'updated_at': instance.updatedAt,
+      'created_by': instance.createdBy,
+      'updated_by': instance.updatedBy,
+    };
---
+++ lib/models/workspace_model.g.dart
@@ -0,0 +1,30 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'workspace_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$WorkspaceModelImpl _$$WorkspaceModelImplFromJson(Map<String, dynamic> json) =>
+    _$WorkspaceModelImpl(
+      tableName: json['table_name'] as String?,
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+      users: (json['users'] as List<dynamic>?)
+          ?.map((e) => UserModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      userRole: json['user_role'] == null
+          ? null
+          : UserRoleModel.fromJson(json['user_role'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$WorkspaceModelImplToJson(
+        _$WorkspaceModelImpl instance) =>
+    <String, dynamic>{
+      'table_name': instance.tableName,
+      'id': instance.id,
+      'name': instance.name,
+      'users': instance.users,
+      'user_role': instance.userRole,
+    };
