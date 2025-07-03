bd027bfbdf88047aedb72537026fe2ade98a76b6
chothip <chothip.ou@gmail.com>
2025-07-03 23:04:09 +0700

    WIP UI Screen Update

--- lib/screens/home/providers/apis/home_api.dart
+++ lib/screens/home/providers/apis/home_api.dart
@@ -1,4 +1,4 @@
-import 'package:dio/dio.dart';
+/*import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project/models/workspace_model.dart';
 import 'package:project/utils/services/rest_api_service.dart';
@@ -35,4 +35,27 @@ class GetWorkspaceByUserApi {
 // Provider สำหรับเรียกใช้ API
 final apiGetWorkspaceByUser = Provider<GetWorkspaceByUserApi>(
   (ref) => GetWorkspaceByUserApi(ref: ref),
-);
+);*/
+
+
+import 'package:dio/dio.dart';
+import 'package:project/components/export.dart';
+import 'package:project/models/workspace_model.dart';
+import 'package:project/utils/services/rest_api_service.dart';
+
+class GetWorkspaceByUserApi {
+  final Ref ref;
+  final String _path = 'master_data/get_workspace_by_user';
+  GetWorkspaceByUserApi({required this.ref});
+  Future<List<WorkspaceModel>> get() async {
+    try {
+      Response response = await ref.read(apiClientProvider).get(_path);
+      List<Map<String, dynamic>> datas = List<Map<String, dynamic>>.from(response.data);
+      return datas.map((data) => WorkspaceModel.fromJson(data)).toList();
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
+
+final apiGetWorkspaceByUser = Provider<GetWorkspaceByUserApi>((ref) => GetWorkspaceByUserApi(ref: ref));
--- lib/screens/home/views/workspace_card.dart
+++ lib/screens/home/views/workspace_card.dart
@@ -1,88 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:project/models/workspace_model.dart';
 
-class WorkspaceCard extends StatelessWidget {
+class WorkspaceCard extends StatefulWidget {
   final WorkspaceModel workspace;
 
   const WorkspaceCard({super.key, required this.workspace});
 
   @override
+  State<WorkspaceCard> createState() => _WorkspaceCardState();
+}
+
+class _WorkspaceCardState extends State<WorkspaceCard> {
+  bool isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    final user = workspace.users != null && workspace.users!.isNotEmpty ? workspace.users![0] : null;
+    final user = widget.workspace.users != null && widget.workspace.users!.isNotEmpty
+        ? widget.workspace.users![0]
+        : null;
 
-    return Container(
-      width: 200,
-      height: 120,
-      margin: const EdgeInsets.only(right: 16),
-      decoration: BoxDecoration(
-        color: Colors.grey[800],
-        borderRadius: BorderRadius.circular(12),
-        image: user != null && user.image != null
-            ? DecorationImage(
-                image: NetworkImage(user.image!),
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.4),
-                  BlendMode.darken,
-                ),
-              )
-            : null,
-      ),
-      child: Stack(
-        children: [
-          // Tag Template
-          Positioned(
-            top: 8,
-            left: 8,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(8, 52, 52, 52).withOpacity(0.8),
-                borderRadius: BorderRadius.circular(6),
-              ),
-              child: const Text(
-                'Template',
-                style: TextStyle(fontSize: 11, fontWeight: FontWeight.w100, color: Colors.white),
-              ),
-            ),
+    return MouseRegion(
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() => isHovered = false),
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+        width: 240,
+        height: 140,
+        margin: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: isHovered
+                ? const Color.fromARGB(162, 114, 133, 227) // ฟ้าสวยเวลา Hover
+                : const Color.fromARGB(189, 220, 227, 230), // โทนเทาฟ้าอ่อนปกติ
+            width: 5,
           ),
-
-          // ชื่อ workspace ตรงกลาง
-          Center(
-            child: Text(
-              workspace.name ?? '',
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-
-          // Avatar + ID มุมล่างซ้าย
-          Positioned(
-            bottom: 8,
-            left: 8,
-            child: Row(
-              children: [
-                if (user != null && user.image != null)
-                  CircleAvatar(
-                    radius: 12,
-                    backgroundImage: NetworkImage(user.image!),
+          boxShadow: isHovered
+              ? [
+                  BoxShadow(
+                    color: const Color.fromARGB(255, 109, 110, 110).withOpacity(0.3),
+                    blurRadius: 11,
+                    offset: const Offset(0, 6),
                   ),
-                const SizedBox(width: 6),
-                Text(
-                  'ID: ${workspace.id ?? "-"}',
-                  style: const TextStyle(
-                    color: Colors.white70,
-                    fontSize: 12,
+                ]
+              : [],
+          image: user != null && user.image != null
+              ? DecorationImage(
+                  image: NetworkImage(user.image!),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.3),
+                    BlendMode.darken,
+                  ),
+                )
+              : null,
+          color: const Color.fromARGB(65, 173, 215, 240), // พื้นหลังกรณีไม่มีภาพ
+        ),
+        child: Stack(
+          children: [
+            //  Tag มุมบนซ้าย
+            Positioned(
+              top: 12,
+              left: 12,
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 11, vertical: 4),
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Text(
+                  'Template',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontWeight: FontWeight.w300,
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
+
+            //  ชื่อ Workspace ตรงกลาง
+            Center(
+              child: Text(
+                widget.workspace.name ?? '',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+            //  Avatar + ID มุมล่างซ้าย
+            Positioned(
+              bottom: 12,
+              left: 12,
+              child: Row(
+                children: [
+                  if (user != null && user.image != null)
+                    CircleAvatar(
+                      radius: 14,
+                      backgroundImage: NetworkImage(user.image!),
+                    ),
+                  const SizedBox(width: 8),
+                  Text(
+                    'ID: ${widget.workspace.id ?? "-"}',
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontSize: 12,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
