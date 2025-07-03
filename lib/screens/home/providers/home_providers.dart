7625d48220ca83a86ccd2d79a6f341dc6f4c26f2
chothip <chothip.ou@gmail.com>
2025-07-03 11:02:25 +0700

    deleete file

--- lib/screens/home/providers/home_providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:project/models/workspace_model.dart';
-import 'package:project/screens/home/apis/home_api.dart';
-
-// Provider สำหรับดึงข้อมูล workspace หลายรายการ (List)
-final homeProvider = FutureProvider<List<WorkspaceModel>>((ref) async {
-  return await ref.read(apiWorkSpace).fetch();
-});
