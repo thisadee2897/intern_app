76f87fc0566c158d0b7220d5a76488264355df63
odin.dev <thisadee.burom@gmail.com>
2025-08-15 15:27:51 +0700

    data api

--- lib/screens/project/project_datail/providers/controllers/piority_breakdown_controller.dart
+++ lib/screens/project/project_datail/providers/controllers/piority_breakdown_controller.dart
@@ -1,18 +1,18 @@
-
 import 'package:project/apis/project_data/priority_breakdown.dart';
 import 'package:project/components/export.dart';
 import 'package:project/models/priority_model.dart';
+import 'package:project/screens/project/sprint/providers/controllers/sprint_controller.dart';
 
 class PiorityBreakdownController extends StateNotifier<AsyncValue<List<PriorityModel>>> {
   final Ref ref;
   PiorityBreakdownController(this.ref) : super(const AsyncValue.loading());
   Future<void> getData() async {
-    try {
-      final result = await ref.read(apiPriorityBreakdown).get();
-      state = AsyncValue.data(result);
-    } catch (e, st) {
-      state = AsyncValue.error(e, st);
-    }
+    state = await AsyncValue.guard(() async {
+      var projectId = ref.read(selectProjectIdProvider);
+      if (projectId == null) return [];
+      final result = await ref.read(apiPriorityBreakdown).get(projectHDId: projectId);
+      return result;
+    });
   }
 }
 
--- lib/screens/project/project_datail/providers/controllers/team_workload_controller.dart
+++ lib/screens/project/project_datail/providers/controllers/team_workload_controller.dart
@@ -1,18 +1,18 @@
-
 import 'package:project/apis/project_data/team_workload.dart';
 import 'package:project/components/export.dart';
 import 'package:project/models/team_workload_model.dart';
+import 'package:project/screens/project/sprint/providers/controllers/sprint_controller.dart';
 
 class TeamWorkloadController extends StateNotifier<AsyncValue<List<TeamWorkloadModel>>> {
   final Ref ref;
   TeamWorkloadController(this.ref) : super(const AsyncValue.loading());
   Future<void> getData() async {
-    try {
-      final result = await ref.read(apiTeamWorkload).get();
-      state = AsyncValue.data(result);
-    } catch (e, st) {
-      state = AsyncValue.error(e, st);
-    }
+    state = await AsyncValue.guard(() async {
+      var projectId = ref.read(selectProjectIdProvider);
+      if (projectId == null) return [];
+      final result = await ref.read(apiTeamWorkload).get(projectHDId: projectId);
+      return result;
+    });
   }
 }
 
